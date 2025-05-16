0c275b6d9ee8ac175288a9b64a30267a8442623f
diego <diegofonterosa@gmail.com>
2025-05-16 19:58:01 +0200

    Cambios

--- Iniciandojs/js/javascript.js
+++ Iniciandojs/js/javascript.js
@@ -20,7 +20,7 @@ function sumar(){
     let n1 = document.getElementById("num1").value
     let n2 = document.getElementById("num2").value
 
-    let suma = parseInt(n1 + n2);
+    let suma = parseInt(n1) + parseInt(n2);
 
     document.getElementById("resultado").textContent = 
 }
