4bbf8d32767aade46df07c6c6122f27a831a7eb3
diego <diegofonterosa@gmail.com>
2025-05-16 19:59:16 +0200

    cambios

--- Iniciandojs/js/javascript.js
+++ Iniciandojs/js/javascript.js
@@ -17,8 +17,8 @@ document.write("Hola mundo");
 
 function sumar(){
     // 1º Obtener los valores
-    let n1 = document.getElementById("num1").value
-    let n2 = document.getElementById("num2").value
+    let n1 = document.getElementById("num1").value;
+    let n2 = document.getElementById("num2").value;
 
     let suma = parseInt(n1) + parseInt(n2);
 
