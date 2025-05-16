c464da7413a48fed592c8253588bf187c0a90be1
diego <diegofonterosa@gmail.com>
2025-05-16 19:50:24 +0200

    Clase 16/05

---
+++ Iniciandojs/js/javascript.js
@@ -0,0 +1,26 @@
+/*
+// Forma 1 usando alert()
+
+alert("Hola mundo");
+
+//Forma 2 usando console.log()
+
+console.log("Hola mundo");
+
+// Forma 3 usando document.write()
+
+document.write("Hola mundo");
+*/
+// Funciones en java
+// Una función es un bloque de código que  no se ejecuta
+// hasta ser llamado
+
+function sumar(){
+    // 1º Obtener los valores
+    let n1 = document.getElementById("num1").value
+    let n2 = document.getElementById("num2").value
+
+    let suma = parseInt(n1 + n2);
+
+    document.getElementById("resultado").textContent = 
+}
